b9d702168c51502ed0d0b54a18c35a75b3544ee0
Abraham Bisrat <abrahammehari0@gmail.com>
2022-01-18 23:38:26 -0600

    Book implemented fully

--- src/business/Book.java
+++ src/business/Book.java
@@ -1,16 +1,38 @@
 package business;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Book {
-	private String name;
+	private String title;
+	private int iSBN;
+	private List<Member> borrowerList;
+	private List<Author> authors;
 	
 	// package level
-	Book(String thatName){
-		this.name = thatName;
+	Book(String thatTitle, int iSBN){
+		this.title = thatTitle;
+		this.iSBN = iSBN;
+		authors = new ArrayList<>();
+		borrowerList = new ArrayList<>();
 	}
 	
-	public String getName() { return name; }
+	public String getTitle() { return title; }
+	public int getISBN() { return iSBN; }
+	
+	public void addAuthor(Author author) { 
+		authors.add(author);
+	}
+	public void addAuthors(List<Author> authorsList) {
+		authors.addAll(authors);
+	}
+	public void addBorrower(Member thisOne) {
+		borrowerList.add(thisOne);
+	}
 	
 	@Override public String toString() {
-		return name;
+		return "[ Book Title : " + title 
+				+ "\t iSBN id : " + iSBN 
+				+ " ] ";
 	}
 }
--- src/business/Factory.java
+++ src/business/Factory.java
@@ -19,8 +19,8 @@ public final class Factory {
 	public static CheckoutEntry createCheckoutEntry(Stuff stuff, Member member, Book book) {
 		return new CheckoutEntry(stuff, member, book);
 	}
-	public static Book createBook(String name) {
-		return new Book(name);
+	public static Book createBook(String title, int iSBN) {
+		return new Book(title, iSBN);
 	}
 	public static CheckoutRecord createCheckoutRecord() {
 		return new CheckoutRecord();
