58f1d8342b73728793b2b1cc1f62a860c8b84602
Abraham Bisrat <abrahammehari0@gmail.com>
2022-01-19 15:53:31 -0600

    Added JavaFX and libraries

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -4,17 +4,22 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Main extends Application{
-
+	
+	public static void main(String[] args) {
+		Application.launch(args);
+	}
+	
 	@Override public void start(Stage primaryStage) throws Exception {
 		Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
-		primaryStage.setTitle("Login form");
-		primaryStage.setScene(new Scene(root, 400, 700));
+		primaryStage.setTitle("Hellow There!");
+		primaryStage.setScene(new Scene(root, 500, 800));
 		primaryStage.show();
-	}
-	public static void main(String[] args) {
-		// code goes here
 	}
 }
